36a4967950a4e6789f3c6d44a993297b5cd02bcf
Anthony Dcosta <adcosta2000@gmail.com>
2023-01-18 00:00:45 -0500

    making spriteAnimation array

--- sprite-animation/logic.js
+++ sprite-animation/logic.js
@@ -10,14 +10,43 @@ const spriteWidth = 575;
 const spriteHeight = 523;
 let frameX = 0;
 let frameY = 0;
+let gameFrame = 0;
+const staggerFrame = 5;
+const spriteAnimations = [];
+const animationStates = [
+   {
+      name:'idle',
+      frames: 7
+   }, 
+   {
+      name:'jump',
+      frames: 7
+   }, 
+   
+]
 
+// list of frame locations in the shadow_dogs picture
+animationStates.forEach((state, index) => {
+   let frames = {
+      loc:[]
+   }
+   for (let j = 0; j < state.frames; j++) {
+      let positionX = j * spriteWidth;
+      let positionY = index * spriteHeight;
+      frames.loc.push({x: positionX, y: positionY})
+   }
+
+   spriteAnimations[state.name] = frames;
+})
 
 //animation loop (critically important)
 function animate(){
    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-   ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight ,0 ,0 ,spriteWidth, spriteHeight)
-   if (frameX < 6) frameX++;
-   else frameX = 0;
+   let position = Math.floor(gameFrame/staggerFrame) % spriteAnimations['idle'].loc.length;
+   let frameX = spriteWidth * position;
+   ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight ,0 ,0 ,spriteWidth, spriteHeight)
+   
+   gameFrame++
    requestAnimationFrame(animate)
 }
 
