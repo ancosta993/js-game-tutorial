8d7a38ed9b8eb86115b94da25091641975cf9e7b
Anthony Dcosta <adcosta2000@gmail.com>
2023-01-17 22:28:18 -0500

    frames of an idle animation

---
+++ sprite-animation/logic.js
@@ -0,0 +1,24 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+//image
+const playerImage = new Image();
+playerImage.src = 'shadow_dog.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+
+
+//animation loop (critically important)
+function animate(){
+   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+   ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight ,0 ,0 ,spriteWidth, spriteHeight)
+   if (frameX < 6) frameX++;
+   else frameX = 0;
+   requestAnimationFrame(animate)
+}
+
+animate();
